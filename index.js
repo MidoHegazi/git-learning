8602af37bb20b835892da3dad7b13a020ad49f98
Mohamed <m.hegazi@thecrypto.farm>
2022-12-30 12:09:24 +0100

    third modification adding functions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log("Hello Mido this is Git");
 console.log("added new function in index.js");
+console.log("index.css-has changed we added a style to Paragraphs");
+console.log("index.css adding style to div");
+
 
 
 
